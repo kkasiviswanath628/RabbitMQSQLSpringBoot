a5502e2b31aadcc442978c07bc698ebdf978ffe0
Kasi Viswanath K <kasi.viswanath@karvy.com>
2018-12-15 12:25:21 +0530

    sptingBoot RabbitMQ

---
+++ SpringBootRabbitMQHelloWorld/src/main/java/com/javainuse/Test.java
@@ -0,0 +1,15 @@
+package com.javainuse;
+
+public class Test{
+	
+	
+	public static void main(String[] args) {
+		int []a[],b;
+		a = new int[5][];
+		b= new int[2];
+		System.out.println(a[0].length);
+	}
+
+}
+
+
